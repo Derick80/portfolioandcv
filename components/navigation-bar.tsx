1281b93769b38d9a571f4c05225a77651ddc8e8b
Derick <derickchoskinson@gmail.com>
2025-04-29 10:51:49 -0500

    update nav bar for production

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -36,8 +36,7 @@ export interface NavLink {
 const NavigationBar   = async (
 ) => {
 const session = await auth();
-if(!session) {
-    return null}
+
     return(
         <nav className="w-full bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
         <div className="container mx-auto px-4 py-3 flex items-center justify-between">
