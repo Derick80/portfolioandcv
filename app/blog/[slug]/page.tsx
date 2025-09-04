59c424479bc0fb6843e031b8b588054eef468f48
Derick <derickchoskinson@gmail.com>
2025-09-04 18:00:47 -0500

    update slug and post logic to troubelshott deployments

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -23,16 +23,20 @@ export default async function Page(props: {
   if (!slug) {
     throw new Error("Slug is required");
   }
-  const post = await getPostBySlug(slug.slug);
-  if (!post) {
-    throw new Error("Post not found");
-  }
+  console.log(slug.slug,"Slug:");
+  // const post = await getPostBySlug(slug.slug);
+  // if (!post) {
+  //   throw new Error("Post not found");
+  // }
 
   return (
     <article className=" relative z-10 mx-auto max-w-4xl space-y-4 overflow-auto px-2 py-4 align-middle md:px-0">
-      <h1 className="text-3xl font-bold">{post.title}</h1>
+      {/* <h1 className="text-3xl font-bold">{post.title}</h1>
       <p>{post.slug}</p>
-      {post.rawMdx}
+      {post.rawMdx} */}
+      <h1 className="text-3xl font-bold">Post Title</h1>
+      <p>Post Slug: {slug.slug}</p>
+      <p>Post content will be rendered here.</p>
     </article>
   );
 }
