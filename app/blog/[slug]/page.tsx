2e8cf08704bef11a317856e116d881f984dbada4
Derick <derickchoskinson@gmail.com>
2025-09-29 11:14:40 -0500

    update raw mdx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -24,20 +24,19 @@ export default async function Page(props: {
   if (!slug) {
     throw new Error("Slug is required");
   }
-  console.log(slug.slug,"Slug:");
   const post = await getPostBySlug(slug.slug);
   if (!post) {
     throw new Error("Post not found");
   }
-
+console.log(post,"post");
   return (
     <article className="relative z-10 mx-auto max-w-4xl space-y-4 overflow-auto px-2 py-4 align-middle md:px-0">
     <h1 className="text-3xl font-bold">{post.title}</h1> 
       <p>{post.slug}</p>
-      {post.rawMdx}
       <h1 className="text-3xl font-bold">Post Title</h1>
       <p>Post Slug: {slug.slug}</p>
       <p>Post content will be rendered here.</p>
+      {post.rawMdx}
     </article>
   );
 }
